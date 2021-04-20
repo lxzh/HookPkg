314a8ab48fd444ce1aae727c397f979f96a0c149
lxzh <leijinfeng@geetest.com>
2021-04-20 15:03:57 +0800

    config change

--- hook-plugin/src/main/groovy/com/lxzh123/hooklib/HookExtension.groovy
+++ hook-plugin/src/main/groovy/com/lxzh123/hooklib/HookExtension.groovy
@@ -2,7 +2,6 @@ package com.lxzh123.hooklib
 
 import com.google.gson.Gson
 import com.lxzh123.hooklib.config.MethodItem
-import com.lxzh123.hooklib.config.MethodList
 import com.lxzh123.hooklib.utils.Log
 import com.lxzh123.hooklib.config.HookConfig
 import com.ss.android.ugc.bytex.common.BaseExtension
@@ -41,28 +40,6 @@ class HookExtension extends BaseExtension {
         if (mHookConfig.target == null) {
             mHookConfig.target = new MethodItem()
         }
-
-        HookConfig1 config11 = new HookConfig1()
-        config11.include = new HashMap<>()
-        List<String> SignUtils = new ArrayList<>()
-        SignUtils.add("getPkgName")
-        SignUtils.add("getRawSignature")
-
-        List<String> SignUtils1 = new ArrayList<>()
-        SignUtils1.add("getPkgName1")
-        SignUtils1.add("getRawSignature1")
-
-        Map<String, List<String>> SignUtilsMap = new HashMap<>()
-        SignUtilsMap.put("SignUtils", SignUtils)
-        SignUtilsMap.put("SignUtils1", SignUtils1)
-
-        Map<String, List<String>> SignUtilsMap1 = new HashMap<>()
-        SignUtilsMap1.put("SignUtils1", SignUtils)
-        SignUtilsMap1.put("SignUtils2", SignUtils1)
-
-        config11.include.put("com.lxzh123.library", SignUtilsMap)
-        config11.include.put("com.lxzh123.library1", SignUtilsMap1)
-        Log.i("HookExtension->hookConfig:" + new Gson().toJson(config11))
     }
 
     HookConfig hookConfig() {
